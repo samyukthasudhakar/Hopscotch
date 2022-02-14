b6497be89ee38fe461bd50b74911a8c62f4575ac
Samyuktha Sudhakar <samyuktha.sudhakar@gmail.com>
2022-02-15 00:23:05 +0530

    refactor : fixed in app.js

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-toggleBtn = document.querySelector("#toggle-btn");
-aLink = document.querySelectorAll("a");
-linkBtn = document.querySelectorAll(".link-btn");
-
-aLink = [...aLink]
-linkBtn = [...linkBtn]
+const toggleBtn = document.querySelector("#toggle-btn");
+let optionLabel = document.querySelectorAll(".option-label");
+let scoreContainer = document.querySelectorAll(".score-container");
+optionLabel = [...optionLabel];
+scoreContainer = [...scoreContainer];
 
 toggleBtn.addEventListener("click",()=>{
     if(toggleBtn.value === "dark-mode"){
         toggleBtn.innerHTML = "<img src='assets/images/light_mode.svg'>"
         toggleBtn.value = "light-mode"
         document.body.style.backgroundColor = "#000000e7";
-        document.body.style.color = "#ffffff";        
+        document.body.style.color = "#ffffff";
+        optionLabel.map(item => item.style.color = "black");
+        scoreContainer.map(item => item.style.color = "black");
     }else{
         toggleBtn.innerHTML = "<img src='assets/images/dark_mode.svg'>"
         toggleBtn.value = "dark-mode"
