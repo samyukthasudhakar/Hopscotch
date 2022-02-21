2548c8d4f680b728c1c7a21bc5671fa221ccb909
Samyuktha Sudhakar <samyuktha.sudhakar@gmail.com>
2022-02-21 11:39:14 +0530

    refactor : responsive nav bar

--- app.js
+++ app.js
@@ -19,3 +19,16 @@ toggleBtn.addEventListener("click",()=>{
         document.body.style.color = "black";
     }
 })
+
+menuBtn = document.querySelector(".menu-btn");
+navContainer = document.querySelector(".nav-pill-container");
+
+menuBtn.addEventListener('click',()=>{
+    console.log('clicked')
+    console.log(navContainer.style.display)
+    if (navContainer.style.display === 'none'){
+        navContainer.style.display = 'block'
+    }else{
+        navContainer.style.display = 'none'
+    }
+})
