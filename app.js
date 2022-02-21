eee82777780fbbc1ad21934f54d8dc2bd3d23faa
Samyuktha Sudhakar <samyuktha.sudhakar@gmail.com>
2022-02-21 11:50:36 +0530

    refactor : persistent dark mode

--- app.js
+++ app.js
@@ -4,19 +4,34 @@ let scoreContainer = document.querySelectorAll(".score-container");
 optionLabel = [...optionLabel];
 scoreContainer = [...scoreContainer];
 
+const setDarkMode = () => {
+    toggleBtn.innerHTML = "<img src='assets/images/light_mode.svg'>"
+    toggleBtn.value = "light-mode"
+    document.body.style.backgroundColor = "#000000e7";
+    document.body.style.color = "#ffffff";
+    optionLabel.map(item => item.style.color = "black");
+    scoreContainer.map(item => item.style.color = "black");
+}
+
+const setLightMode = () => {
+    toggleBtn.innerHTML = "<img src='assets/images/dark_mode.svg'>"
+    toggleBtn.value = "dark-mode"
+    document.body.style.backgroundColor = "#ffffff";
+    document.body.style.color = "black";
+    
+}
+
+if(localStorage.getItem('mode')==='dark'){
+    setDarkMode()
+}
+
 toggleBtn.addEventListener("click",()=>{
     if(toggleBtn.value === "dark-mode"){
-        toggleBtn.innerHTML = "<img src='assets/images/light_mode.svg'>"
-        toggleBtn.value = "light-mode"
-        document.body.style.backgroundColor = "#000000e7";
-        document.body.style.color = "#ffffff";
-        optionLabel.map(item => item.style.color = "black");
-        scoreContainer.map(item => item.style.color = "black");
+        setDarkMode()
+        localStorage.setItem('mode','dark')
     }else{
-        toggleBtn.innerHTML = "<img src='assets/images/dark_mode.svg'>"
-        toggleBtn.value = "dark-mode"
-        document.body.style.backgroundColor = "#ffffff";
-        document.body.style.color = "black";
+        setLightMode()
+        localStorage.setItem('mode','light')
     }
 })
 
